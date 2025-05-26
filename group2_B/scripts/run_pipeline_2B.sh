8091035fe8aae18c5e40c94d392d3a560c7d7caf
Maria <mariatc025@gmail.com>
2025-05-26 22:35:46 +0200

    modification to use the test data and the correct classifier

--- group2_B/scripts/run_pipeline_2B.sh
+++ group2_B/scripts/run_pipeline_2B.sh
@@ -1,7 +1,7 @@
-    nextflow run group2_B/scripts/qiime2_pipeline.nf \
-        --reads "group2_B/data/simulated_fastqs/*_{R1,R2}.fastq.gz"\
+nextflow run group2_B/scripts/qiime2_pipeline.nf \
+        --reads "group2_B/data/*_{1,2}.fastq.gz" \
         --outdir "group2_B/results/qiime_output" \
-        --classifier_db "group2_B/databases/Greengenes_16S_2011_1.arb.gz" \
+        --classifier_db "group2_B/classifier/silva-138-99-nb-classifier.qza" \
         --sampling_depth 5000 \
         --denoiser dada2 \
-        -profile standard # Or specify a profile for your execution environment (e.g., docker, singularity)
+        -profile standard
